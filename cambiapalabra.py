744cbe71d11dab636ef58ff5ed244f80f4273fdb
Rubén Pulido Marchal <pulido@MacBook-Air-de-Ruben.local>
2022-08-08 18:54:52 +0200

    Con argumentos

--- cambiapalabra.py
+++ cambiapalabra.py
@@ -1,14 +1,19 @@
-origial = input ("introduce una palabra: ")
-nueva = input ("introduce la nueva palabra: ")
 
-nombreF = "fichero.txt"
+import sys
+
+argumentos = sys.argv
+
+nombreF = argumentos [1]
+original = argumentos [2]
+nueva = argumentos [3]
+
 
 f = open(nombreF, "r")
 
 texto_original = f.read()
 f.close()
 
-texto_sutituido = texto_original.replace(origial, nueva)
+texto_sutituido = texto_original.replace(original, nueva)
 
 f = open(nombreF, "w")
 f.write(texto_sutituido)
