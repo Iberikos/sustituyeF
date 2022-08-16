1743901b16d8c90cf3655b3ea292bbe747118412
Rubén Pulido Marchal <pulido@MacBook-Air-de-Ruben.local>
2022-08-16 19:51:58 +0200

    versión final

--- cambiapalabra.py
+++ cambiapalabra.py
@@ -3,9 +3,20 @@ import sys
 
 argumentos = sys.argv
 
+if len(argumentos) <2:
+    f= input ("Nombre de fichero: ")
+    argumentos.append(f)
+
+if len(argumentos) < 3:
+    argumentos.append (input("palabra original: "))
+
+if len(argumentos) < 4:
+    argumentos.append (input("palabra nueva: "))
+
 nombreF = argumentos [1]
 original = argumentos [2]
 nueva = argumentos [3]
+
 
 
 f = open(nombreF, "r")
